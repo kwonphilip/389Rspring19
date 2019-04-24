d3f6f09bf1308050d146282f41a27aa4fc288a90
Philip Kwon <philipkwon85@gmail.com>
2019-04-23 21:27:23 -0400

    hw9 so far

--- assignments/9_Crypto_I/writeup/crack.py
+++ assignments/9_Crypto_I/writeup/crack.py
@@ -4,9 +4,14 @@ import hashlib
 import string
 
 def crack():
-    hashes = # open and read hashes.txt
-    passwords = # open and read passwords.txt
+    hashes = open('hashes.txt', 'r')
+    passwords = open('passwords.txt', 'r')
     characters = string.ascii_lowercase
+
+    hash_list = []
+
+    for line in hashes:
+    	hash_list.append(line.rstrip())
 
     for c in characters:
         for p in passwords:
@@ -14,6 +19,14 @@ def crack():
 
             # print hashes as 'input:hash'
             # i.e.  yeet:909104cdb5b06af2606ed4a197b07d09d5ef9a4aad97780c2fe48053bce2be52
+
+            cp = c + p.rstrip()
+            cp = cp.encode()
+            hash_object = hashlib.sha256(cp)
+
+            if hash_object in hash_list:
+            	print(p + ":" + hash_object)
+
 
 if __name__ == "__main__":
     crack()
