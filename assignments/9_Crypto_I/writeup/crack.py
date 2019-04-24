01001f0f1cf645d4c98170cc89145a10490f8c6e
Philip Kwon <philipkwon85@gmail.com>
2019-04-24 16:30:44 -0400

    hw9 so far

--- assignments/9_Crypto_I/writeup/crack.py
+++ assignments/9_Crypto_I/writeup/crack.py
@@ -4,37 +4,35 @@ import hashlib
 import string
 
 def crack():
-    hashes = open('hashes.txt', 'r')
-    passwords = open('passwords.txt', 'r')
-    characters = string.ascii_lowercase
+	hashes = open('hashes.txt', 'r')
+	pw = open('passwords.txt', 'r')
+	characters = string.ascii_lowercase
+	
+	hash_list = []
 
-    hash_list = []
+	for line in hashes:
+		hash_list.append(line)
 
-    for line in hashes:
-    	hash_list.append(line)
+	passwords = []
 
-    #for line in hash_list:
-    #	print("hash = " + line)
+	for p in pw:
+		passwords.append(p.rstrip())
 
-    for c in characters:
-        for p in passwords:
-            # crack hashes
+	for c in characters:
+		for p in passwords:
+			#crack hashes
 
-            # print hashes as 'input:hash'
-            # i.e.  yeet:909104cdb5b06af2606ed4a197b07d09d5ef9a4aad97780c2fe48053bce2be52
+			# print hashes as 'input:hash'
+			# i.e.  yeet:909104cdb5b06af2606ed4a197b07d09d5ef9a4aad97780c2fe48053bce2be52
 
-            cp = c + p.rstrip()
-            hash_object = hashlib.sha256(cp.encode()).hexdigest()
+			cp = c + p
+			cp2 = cp.encode()
+			hash_object = hashlib.sha256(cp2).hexdigest()
 
-            #print("cp = " + cp)
-            #print("ho = " + hash_object)
-            print("c = " + c)
-            print("p = " + p)
-            
-            for x in hash_list:
-            	y = int(x, 16)
-            	if y == hash_object:
-            		print(cp + ":" + hash_object)
+			#print("cp = " + cp)
+
+			if hash_object in hash_list:
+				print(cp + ":" + hash_object)
 
 
 if __name__ == "__main__":
